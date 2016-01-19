3c699c9d720a94c328c601efa847c028b65bb566
thenaughtychild <f2014768@pilani.bits-pilani.ac.in>
2016-01-19 21:22:23 +0530

    basic backend

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
 	'facebookAuth' : {
-		'clientID': 'enter client id here',
-		'clientSecret': 'enter client secret here',
-		'callbackURL': 'enter callback here'
+		'clientID': '243089016023303',
+		'clientSecret': 'e13ddb1d924bb7387c7e59bc9e62b71e',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
-		'clientID': 'enter client id here',
-		'clientSecret': 'enter client secret here',
-		'callbackURL': 'enter callback here'
+		'clientID': '738693532334-c4to4fjjbg6gpaf9hq93aimrkoa37eg6.apps.googleusercontent.com',
+		'clientSecret': 'NvM006wMV0icaTtAWxV-nchT',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
 	}
 }
