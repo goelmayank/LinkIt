e7d6bcf4664dcf0482600b0ac70f43d47640919b
thenaughtychild <f2014768@pilani.bits-pilani.ac.in>
2016-02-24 20:43:51 +0530

    R corrected

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -66,9 +66,9 @@ KeyboardInputManager.prototype.listen = function() {
         }
 
         // R key restarts the game
-        if (!modifiers && event.which === 82) {
-            self.restart.call(self, event);
-        }
+        // if (!modifiers && event.which === 82) {
+        //     self.restart.call(self, event);
+        // }
     });
 
     // Respond to button presses
